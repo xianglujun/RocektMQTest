a02b64bedbbcc8f566f3d8b4afa421285ede9664
xianglujun <xianglj1991@163.com>
2018-11-29 16:12:19 +0800

    修改相关注解

--- src/main/java/com/learn/rocketmq/constant/MqConstants.java
+++ src/main/java/com/learn/rocketmq/constant/MqConstants.java
@@ -11,7 +11,8 @@ public class MqConstants {
     /**
      * mqnamesrv的地址和端口
      */
-    public static final String MQ_NAME_SRV_ADDRESS = "192.168.59.201:9876";
+//    public static final String MQ_NAME_SRV_ADDRESS = "192.168.59.201:9876";
+    public static final String MQ_NAME_SRV_ADDRESS = "10.200.173.48:9876";
 //    public static final String MQ_NAME_SRV_ADDRESS = "192.168.3.73:9876";
 //    public static final String MQ_NAME_SRV_ADDRESS = "localhost:9876";
 }
